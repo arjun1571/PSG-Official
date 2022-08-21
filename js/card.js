19d66b4e05dfd1d75e4b96d66463d052fdde27c6
Arjun Das <arjundasback515@gmail.com>
2022-08-22 01:57:57 +0600

    card dynamic added

--- js/card.js
+++ js/card.js
@@ -1,26 +1,40 @@
 const playerObj1={
-    name: "Lionen messi",
-    image: "https://images.unsplash.com/photo-1533473359331-0135ef1b58bf?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=870&q=80"
+    name: "Lionel Messi",
+    image: "https://images.psg.media/media/209026/card-season-22-23-messi.png?center=0.5,0.5&mode=crop&width=400&height=600&quality=75",
+
 }
 const playerObj2={
-    name: "Naymer jr",
-    image: "https://images.unsplash.com/photo-1533473359331-0135ef1b58bf?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=870&q=80"
+    name: "Neymar jr",
+    image: "https://images.psg.media/media/27051/card-season-22-23-neymar-temp.png?center=0.5,0.5&mode=crop&width=400&height=600&quality=75"
 }
 const playerObj3={
-    name: "mbappe",
-    image: "https://images.unsplash.com/photo-1533473359331-0135ef1b58bf?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=870&q=80"
+    name: "Kylian Mbappe",
+    image: "https://images.psg.media/media/27048/card-season-22-23-mbappe.png?center=0.5,0.5&mode=crop&width=400&height=600&quality=75"
+}
+const playerObj4={
+    name: "Keylor Navas",
+    image: "https://images.psg.media/media/33317/card-season-22-23-navas.png?center=0.5,0.5&mode=crop&width=400&height=600&quality=75"
+}
+const playerObj5={
+    name: "Sergio Ramos",
+    image: "https://images.psg.media/media/207480/card-season-22-23-ramos.png?center=0.5,0.5&mode=crop&width=400&height=600&quality=75"
+}
+const playerObj6={
+    name: "Marquinhos",
+    image: "https://images.psg.media/media/27046/card-season-22-23-marquinhos.png?center=0.5,0.5&mode=crop&width=400&height=600&quality=75"
 }
 
-function displayCard(){
+// const playerArray = [playerObj1,playerObj2,playerObj3,playerObj4,playerObj5,playerObj6];
+
+function displayCard(player){
     const cardSection = document.getElementById("card-detail");
     const createDiv = document.createElement("div");
     createDiv.innerHTML=`
-    <div class="card me-2" style="width: 16rem;">
-                            <img src="..." class="card-img-top" alt="...">
+    <div class="card me-2 mb-3" style="width: 16rem;">
+                            <img src=${player.image} class="card-img-top" alt="...">
                             <div class="card-body">
-                                <h5 class="card-title">Card title</h5>
-                                <p class="card-text">Some quick example text to build on the card title and make up the
-                                    bulk of the card's content.</p>
+                                <h5 class="card-title">${player.name}</h5>
+                                <p class="card-text"> </p>
                                 <a href="#" class="btn btn-primary">Go somewhere</a>
                             </div>
                         </div>
@@ -28,12 +42,16 @@ function displayCard(){
       `
     cardSection.appendChild(createDiv);
 }
-displayCard()
-displayCard()
-displayCard()
-displayCard()
-displayCard()
-displayCard()
+
+
+displayCard(playerObj1)
+displayCard(playerObj2)
+displayCard(playerObj3)
+displayCard(playerObj4)
+displayCard(playerObj5)
+displayCard(playerObj6)
+
+
 
 
 
