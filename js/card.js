f23e7dae61456820309884a811887a18e1618dd1
Arjun Das <arjundasback515@gmail.com>
2022-08-22 01:29:20 +0600

    card section added

--- js/card.js
+++ js/card.js
@@ -1,1 +1,42 @@
-console.log("heloo card");
+const playerObj1={
+    name: "Lionen messi",
+    image: "https://images.unsplash.com/photo-1533473359331-0135ef1b58bf?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=870&q=80"
+}
+const playerObj2={
+    name: "Naymer jr",
+    image: "https://images.unsplash.com/photo-1533473359331-0135ef1b58bf?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=870&q=80"
+}
+const playerObj3={
+    name: "mbappe",
+    image: "https://images.unsplash.com/photo-1533473359331-0135ef1b58bf?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=870&q=80"
+}
+
+function displayCard(){
+    const cardSection = document.getElementById("card-detail");
+    const createDiv = document.createElement("div");
+    createDiv.innerHTML=`
+    <div class="card me-2" style="width: 16rem;">
+                            <img src="..." class="card-img-top" alt="...">
+                            <div class="card-body">
+                                <h5 class="card-title">Card title</h5>
+                                <p class="card-text">Some quick example text to build on the card title and make up the
+                                    bulk of the card's content.</p>
+                                <a href="#" class="btn btn-primary">Go somewhere</a>
+                            </div>
+                        </div>
+    
+      `
+    cardSection.appendChild(createDiv);
+}
+displayCard()
+displayCard()
+displayCard()
+displayCard()
+displayCard()
+displayCard()
+
+
+
+
+
+
